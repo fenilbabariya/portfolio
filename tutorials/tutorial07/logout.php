fb14ecdb8483acdfb3b263d1d558545ac6e8f31e
Fenil Babariya <fbabariya323@rku.ac.in>
2022-01-14 15:47:00 +0530

    07 complete

--- tutorials/tutorial07/logout.php
+++ tutorials/tutorial07/logout.php
@@ -1,16 +1,8 @@
 <?php
 
-if (isset($_COOKIE['username']) && (isset($_COOKIE['password'])))
-  {
-      $username=$_COOKIE['username'];
-      $password=$_COOKIE['password'];
-
-      setcookie('username',$username,time()-1);
-      setcookie('password',$password,time()-1);
-  }
  session_start();
 
   echo '<script type="text/javascript"> alert("Logout Successfully... ");</script>';
-  session_destroy();   // function that Destroys Session 
+  // session_destroy();   // function that Destroys Session 
   header("Location: login.php");
 ?>
